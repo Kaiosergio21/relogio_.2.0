9a98e6dc2f93ac7204f0e3b6019536465980a3ef
Kaio Sérgio Sales Nunes <137112682+Kaiosergio21@users.noreply.github.com>
2024-03-27 10:31:36 -0300

    Add files via upload

---
+++ Relogio oficial/Script/index.js
@@ -0,0 +1,40 @@
+// Kaio sergio, caio jmustino, viniccius nascimento
+let horas = document.getElementById('horas')
+let minutos = document.getElementById('minutos')
+let segundos = document.getElementById('segundos')
+let ampm = document.getElementById('ampm')
+
+let hh = document.getElementById('hh')
+let mm = document.getElementById('mm')
+let ss = document.getElementById('ss')
+
+let hr_pontos = document.getElementById('hr_ponto')
+let min_pontos = document.getElementById('min_ponto')
+let seg_pontos = document.getElementById('sec_ponto')
+
+setInterval(() => {
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let amdDmd = h >= 12 ? "PM" : "AM"
+    const formatTime = n => n < 10 ? n = `0${n}` : n
+    
+    if (h >= 12) h = h - 12
+
+    h=(h<10)? '0'+h:h;
+    m=(m<10)? '0'+m:m;
+    s=(s<10)? '0'+s:s;
+    
+    horas.innerHTML = h + `<br><span>Horas</span>`
+    minutos.innerHTML = m + `<br><span>Minutos</span>`
+    segundos.innerHTML = s + `<br><span>Segundos</span>`
+    ampm.textContent = amdDmd
+
+    hh.style.strokeDashoffset = 440 - (440 * h) / 12
+    mm.style.strokeDashoffset = 440 - (440 * m) / 60
+    ss.style.strokeDashoffset = 440 - (440 * s) / 60
+
+    hr_pontos.style.transform = `rotate(${h * 30}deg)`
+    min_pontos.style.transform = `rotate(${m * 6}deg)`
+    seg_pontos.style.transform = `rotate(${s * 6}deg)`
+})
